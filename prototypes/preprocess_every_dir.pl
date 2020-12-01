da3d2dbb1c15f6d7aa94c210608484f572aafd26
jonasbn <jonasbn@gmail.com>
2020-12-01 21:59:53 +0100

    Removed some cruft, so we focus on the File::Find part

--- prototypes/preprocess_every_dir.pl
+++ prototypes/preprocess_every_dir.pl
@@ -5,41 +5,25 @@ use warnings;
 use v5.10; # say
 use File::Find; # find
 use Data::Dumper;
-use Text::Gitignore qw(match_gitignore build_gitignore_matcher);
-use File::Slurper qw(read_lines);
 
-my $matcher = build_gitignore_matcher([]);
 
-find({ wanted => \&process, preprocess => \&preprocess }, $ARGV[0]);
+find({ wanted => \&process, preprocess => \&preprocess, postprocess => \&postprocess }, $ARGV[0]);
 
 exit 0;
 
-sub process {
-    $matcher->($_) && ($File::Find::prune = 1) || say STDERR "Examining: $_";
-
-    #say 'we are in process';
-    #say STDERR "Examining: $_";
-    
-    #if ($m->{matcher} and $m->{matcher}->($_)) {
-    #    say "Please ignore: local";
-    #} else {
-    #    say "Proceed with $_";
-    #}
-}
-
 sub preprocess {
-    
-    #say 'we are in preprocess';
-    #say STDERR "Examining: $_";
-
-    my @lines = ();
-
-    if (-e '.yakignore' and -f _) {
-        @lines = read_lines('.yakignore');
-        say "Hurrah we found a yak in $File::Find::dir";
-        $matcher = build_gitignore_matcher([@lines]);
-        #$m->{matcher} = $matcher;
-    }
+    say 'we are in preprocess';
+    say STDERR "Examining: $File::Find::dir";
 
     return @_;
+}
+
+sub process {
+    say 'we are in process';
+    say STDERR "Examining: $File::Find::dir";
+}
+
+sub postprocess {
+    say 'we are in postprocess';
+    say STDERR "Examining: $File::Find::dir";
 }
