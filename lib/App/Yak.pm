e22762ce0a4f8499cfeae703882d40dd2b54de06
jonasbn <jonasbn@gmail.com>
2020-12-07 20:26:34 +0100

    Minor adjustments and clarifications to documentation

--- lib/App/Yak.pm
+++ lib/App/Yak.pm
@@ -497,4 +497,414 @@ sub _set_emoji {
 
 1;
 
-# ABSTRACT: turns baubles into trinkets
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=begin markdown
+
+[![Build Status](https://travis-ci.org/jonasbn/perl-app-yak.svg?branch=master)](https://travis-ci.org/jonasbn/yak)
+![Spellcheck Action](https://github.com/jonasbn/perl-app-yak/workflows/Spellcheck%20Action/badge.svg)
+![Markdownlint Action](https://github.com/jonasbn/perl-app-yak/workflows/Markdownlint%20Action/badge.svg)
+
+The **yak** command line utility is still WIP and to be regarded as *alpha* software, most features are working but it not ready for first official release
+
+---
+
+=end markdown
+
+=head1 yak
+
+B<App::Yak> - application to help with yak shaving for Git repositories etc.
+
+=head1 SYNOPSIS
+
+    my $yak->new();
+    my $rv = $yak->process(); 
+
+=head1 DESCRIPTION
+
+The B<yak> I<shaver> can scan a directory for files, which can be classified as yaks in need of shaving. Meaning files which are maintained else where are often copy-pasted.
+
+The file names can be configured in a central configuration file, like this:
+
+F<$HOME/.config/yak/checksums.json>
+
+    {
+        "CONTRIBUTING.md": "15701b6b27e1d49ca6636f2695cfc49b6622c7152f74b14becc53850811db54f"
+    }
+
+If a file is encountered, which matches the name, the checksum of the encountered file is calculated and is compared to the checksum listed in the central file.
+
+=over
+
+=item * If they match, everything is okay
+
+=item * If they differ, the difference has to be addressed
+
+=back
+
+The recommendation is to have the checksum in the central file, reflect the authoritative revision and hence you can overwrite the file in the directory you where inspecting.
+
+Alternatively to specifying a checksum, you can specify a file URL:
+
+    {
+        "MANIFEST.SKIP": "file://MANIFEST.SKIP"
+    }
+
+The file pointed to has to be available in: F<$HOME/.config/yak/files>
+
+Then C<yak> can calculate the checksum dynamically, based on the reference file and can based on invocation copy the reference file to the location of the evaluated file in the case where the two differ.
+
+=head2 CHECKSUM DATA FILE EXAMPLE
+
+This JSON file should be created as C<$HOME/.config/yak/checksums.json>.
+
+    {
+        "CODE_OF_CONDUCT.md": "da9eed24b35eed80ce28e07b02725dbb356cfa56500a1552a1410ab5c73af82c",
+        "CONTRIBUTING.md": "file://CONTRIBUTING.md",
+        "PULL_REQUEST_TEMPLATE.md": "91dabee84afd46f93894d1a266a773f3d46c2c0b1ae4813f0c7dba34df1dc260",
+        "MANIFEST.SKIP": "file://MANIFEST.SKIP"
+    }
+
+=head2 IGNORING CERTAIN DIRECTORIES, FILES AND FILENAME PATTERNS
+
+C<yak> supports the ability to ignore:
+
+=over
+
+=item * Files
+
+=item * Directories
+
+=item * Filename patterns
+
+=back
+
+This is accomplished using an implementation based on C<.gitignore>. To not intervene and to let C<git> and C<yak> work in harmony. The files used by C<yak> are named C<.yakignore>.
+
+The mean that you can:
+
+=over
+
+=item * Specify patterns of files and directories in your configuration file, see L</CONFIGURATION>. This configuration will be overwritten if the next options are used.
+
+=item * You can add an C<.yakignore> in the root of your repository and it will work for all files and directories in the file structure beneath it. Do note that the presence of this files, ignores and configuration in regard to using this feature. Meaning that disabling C<yak> ignores for a single repository can be accomplished by placing an empty C<.yakignore> file in the root of the repository.
+
+=item * a I<child> C<.yakignore> can be placed in a subsequent directory, working on all files and directories beneath it, do note that directories specified to be ignored in the I<parent> C<.yakignore> are ignored and are not parsed and used.
+
+=back
+
+=head2 YAK IGNORE FILE EXAMPLE
+
+    .git
+    local
+
+The above example specified the C<local> directory created by Perl's Carton. Another good candidate could be the C<.git> folder.
+
+Since C<yak> is processing a directory structure recursively, specifying directories should speed up the processing. Specifying single files by name can be used to skip a file specified in the data source file temporarily.
+
+=head1 INVOCATION
+
+C<yak> takes the following command line arguments:
+
+=over
+
+=item * C<--verbose>, enables more verbose output, can be configured see L</CONFIGURATION>
+
+=item * C<--silent>, disables output and you have to rely on the return value see L</RETURN VALUES> below.
+
+=item * C<--debug>, enables debug output. can be configured see L</CONFIGURATION>
+
+=item * C<--nodebug>, disables debug output even if confgured or provided as C<--debug>, see above
+
+=item * C<--config file>, reads alternative configuration file instead of default, see L</CONFIGURATION>
+
+=item * C<--noconfig>, disables reading of the configuration file, (see L</CONFIGURATION>) and you have to rely on the command line arguments
+
+=item * C<--nochecksums>, disables reading of the global checksums file, see L</DATA SOURCE>
+
+=item * C<--checksums file>, reads alternative checksums file instead of default, see L</DATA SOURCE>
+
+=item * C<--color>, enables colorized output, enabled by default or can be configured, see L</CONFIGURATION>
+
+=item * C<--nocolor>, disables colorized output, even if confgured or provided as C<--color>, see above
+
+=item * C<--emoji>, enables emojis in output, enabled by default or can be configured, see L</CONFIGURATION>
+
+=item * C<--noemoji>, disables emojis in output, even if confgured or provided as C<--emoji>, see above
+
+=item * C<--about>, emits output on configuration and invocation and terminates with success
+
+=item * C<--help>, emits help message listing all available options
+
+=back
+
+Command line arguments override the configuration.
+
+=head2 RETURN VALUES
+
+=over
+
+=item * C<0>, success, everything is okay
+
+=item * C<1>, failure, a located filed did not match the designated checksum
+
+=back
+
+Note that C<--about> return as success with out processing any data apart from reading configuration and parsing command line arguments.
+
+=head1 ENVIRONMENT
+
+C<yak> supports the following environment variables:
+
+=over
+
+=item * C<$CLICOLOR}>, if set to false (C<0>) it attempts to disable colorized output, if set to true (C<1>), it attempts to enable colorized output
+
+=item * C<$CLICOLOR_FORCE>, if set to true (C<1>) it enables colorized output, if set to false (C<1>), it disables colorized output
+
+=back
+
+The order of precedence is as follows:
+
+=over
+
+=item 1. Environment (this section), C<$CLICOLOR_FORCE>
+
+=item 2. Command line arguments, C<--nocolor> and C<--color> in that order, see L</INVOCATION>
+
+=item 3. Environment (this section), C<$CLICOLOR>
+
+=item 4. Configuration, see L</CONFIGURATION>, C<color> configuration option
+
+=back
+
+This aims to follow the proposed standard described in L<this article|https://bixense.com/clicolors/>.
+
+=head1 CONFIGURATION
+
+F<$HOME/.config/yak/config.yml>
+
+C<yak> can be configured using the following paramters:
+
+=over
+
+=item * C<verbose>, enabling (C<true>) or disabling (C<false>) more verbose output
+
+=item * C<debug>, enabling (C<true>) or disabling (C<false>) debug output
+
+=item * C<color>, enabling (C<true>) or disabling (C<false>) colorized output
+
+=item * C<emoji>, enabling (C<true>) or disabling (C<false>) colorized output
+
+=item * C<success_emoji>
+
+=item * C<failure_emoji>
+
+=item * C<skip_emoji>
+
+=item * C<yakignores>, specify a list of file directory names and patterns to be ignored
+
+=back
+
+Configuration can be overridden by command line arguments, see L</INVOCATION>.
+
+=head2 EXAMPLE CONFIGURATION
+
+This YAML file should be created as C<$HOME/.config/yak/config.yml>.
+
+    verbose: false
+    debug: false
+    skip_emoji: ✖️
+    failure_emoji: ❌
+    success_emoji: ✅
+    yakignores:
+    - .git
+    - local
+
+=head1 DATA SOURCE
+
+There are 3 ways to provide checksum data to C<yak>.
+
+=over
+
+=item * The default using: C<$HOME/.config/yak/checksums.json>, which can then be edited to match your needs
+
+=item * Using a project or repository specific: C<.yaksums.json> located in the root of your project or repository directory
+
+=item * Using an JSON file adhering to formatting described in this chapter, which can be located elsewhere on your file system
+
+=back
+
+The default data source is described in the L</DESCRIPTION>. As a an alternative a per project file can be specified in the designated repository/directory.
+
+The file should be named: C<.yaksums.json>
+
+The contents follow the same format as the C<$HOME/.config/yak/checksums.json>.
+
+This JSON file should look as follows:
+
+    {
+        "<filename>": "<sha256 checksum for the file specifed by the filename>"
+    }
+
+An example:
+
+    {
+        "CODE_OF_CONDUCT.md": "da9eed24b35eed80ce28e07b02725dbb356cfa56500a1552a1410ab5c73af82c",
+        "CONTRIBUTING.md": "file://CONTRIBUTING.md",
+        "PULL_REQUEST_TEMPLATE.md": "91dabee84afd46f93894d1a266a773f3d46c2c0b1ae4813f0c7dba34df1dc260",
+        "MANIFEST.SKIP": "file://MANIFEST.SKIP"
+    }
+
+If you want to have B<Yak> help you checking for the presence of a file, specify the I<boolean> C<true> instead of a checksum.
+
+    {
+        "ISSUE_TEMPLATE.md": true,
+        "README.md": true
+    }
+
+Or you can issue an error if a file is present, which should not be there, again using a I<boolean>, but set to C<false>.
+
+    {
+        ".vstags": false
+    }
+
+=head1 USING DOCKER
+
+An experimental Docker implementation has been included with the repository.
+
+It can be built using the following statement:
+
+    $ docker build -t jonasbn/yak .
+
+And then run as follows:
+
+    $ docker run --rm -it -v $PWD:/tmp jonasbn/yak
+
+It will consume all the command line arguments (see L</INVOCATION>).
+
+The Docker image has the following command line arguments embedded:
+
+=over
+
+=item * C<--noconfig>
+
+=item * C<--nochecksums>
+
+=back
+
+Since the ability to read files outside the Docker container is limited to mounted directories.
+
+The mount point is expected to be a directory containing the files to be checked against the checksum data structure. Please see the L</LIMITATIONS> for details.
+
+If you want to utilize the supported environment variables (see L</ENVIRONMENT>) you have to do something along the lines of:
+
+    $ docker run --rm -it -v $PWD:/tmp --env CLICOLOR=$CLICOLOR jonasbn/yak
+
+=head1 REQUIREMENTS AND DEPENDENCIES
+
+C<yak> is specified to a minimum requirement of Perl 5.10, based on an analysis made using L<Perl::MinimumVersion>, implementation syntax requires Perl 5.8.0, so C<yak> I<could be made to work> for 5.8.0.
+
+=over
+
+=item * L<Crypt::Digest::SHA256|https://metacpan.org/pod/CryptX>
+
+=item * L<Data::Dumper|https://metacpan.org/pod/Data::Dumper>
+
+=item * L<Data::Stack|https://metacpan.org/pod/Data::Stack>
+
+=item * L<Env|https://metacpan.org/pod/Env>
+
+=item * L<File::Find|https://metacpan.org/pod/File::Find>
+
+=item * L<File::Slurper|https://metacpan.org/pod/File::Slurper>
+
+=item * L<Getopt::Long|https://metacpan.org/pod/Getopt::Long>
+
+=item * L<JSON|https://metacpan.org/pod/JSON>
+
+=item * L<List::MoreUtils|https://metacpan.org/pod/List::MoreUtils>
+
+=item * L<Term::ANSIColor|https://metacpan.org/pod/Term::ANSIColor>
+
+=item * L<Text::Gitignore|https://metacpan.org/pod/Text::Gitignore>
+
+=item * L<YAML::Tiny|https://metacpan.org/pod/YAML::Tiny>
+
+=back
+
+=head1 LIMITATIONS
+
+=over
+
+=item * C<yak> is specified to a minimum requirement of Perl 5.10, based on an analysis made using L<Perl::MinimumVersion>, implementation syntax requires Perl 5.8.0, so C<yak> I<could be made to work> for 5.8.0.
+
+=item * Running under Docker is limited to using only checksums specified in a local <.yaksums.json> and configuration has to be specified using command line arguments not a file
+
+=item * The use of a local: C<.yaksums.json> is limited to checksums and cannot calculate based on files, since files are located in an unmounted directory
+
+=item * The use of YAML implementation is based on L<YAML::Tiny|https://metacpan.org/pod/YAML::Tiny> and is therefor limited to this more simple implementation, which was however deemed sufficient for B<Yak>.
+
+=item * C<yak> does currently not support symbolic links when doing file system traversal. The implementation is based on L<File::Find|https://metacpan.org/pod/File::Find> and support for symbolic links could be enabled, but has not been regarded as necessary for now.
+
+=item * The parsing of C<.yakignore> files is based on L<Text::Gitignore|https://metacpan.org/pod/Text::Gitignore> and is limited to what this implementation supports, no known issues at this time.
+
+=back
+
+=head1 ISSUE REPORTING
+
+If you experience any issues with B<yak> report these via GitHub. Please read L<the issue reporting template|https://github.com/jonasbn/perl-app-yak/blob/master/.github/ISSUE_TEMPLATE.md>.
+
+=head1 DEVELOPMENT
+
+If you want to contribute to B<yak> please read the L<Contribution guidelines|https://github.com/jonasbn/perl-app-yak/blob/master/CONTRIBUTING.md>
+and follow L<the pull request guidelines|https://github.com/jonasbn/yak/blob/master/.github/PULL_TEMPLATE.md>.
+
+=head1 MOTIVATION
+
+Much of what I do is yak shaving. For you who are not familiar with the term:
+
+    "[MIT AI Lab, after 2000: orig. probably from a Ren & Stimpy episode.]
+    Any seemingly pointless activity which is actually necessary to solve
+    a problem which solves a problem which, several levels of recursion
+    later, solves the real problem you're working on."
+
+REF: L<The Jargon File|http://www.catb.org/~esr/jargon/html/Y/yak-shaving.html>
+
+Used commonly for repetive and boring work, required to reach a certain goal.
+
+=head1 AUTHOR
+
+=over
+
+=item * jonasbn, L<website|https://jonasbn.github.io/>
+
+=back
+
+=head1 COPYRIGHT
+
+C<yak> is (C) by Jonas Brømsø, (jonasbn) 2018-2020
+
+L<Image|https://unsplash.com/photos/3b3O75X0Jzg> used on the B<yak> L<website|https://jonasbn.github.io/perl-app-yak/> is under copyright by L<Shane Aldendorff|https://unsplash.com/@pluyar>.
+
+=head1 LICENSE
+
+C<yak> is released under the MIT License
+
+=head1 REFERENCES
+
+=over
+
+=item 1. GitHub: L<"The Yak Project"|https://jonasbn.github.io/yak/>
+
+=item 2. MetaCPAN: L<TERM-ANSICOLOR|https://metacpan.org/pod/Term%3A%3AANSIColor>
+
+=item 3. Bixsense: L<"Standard for ANSI Colors in Terminals"|https://bixense.com/clicolors/>
+
+=back
+
+=cut
